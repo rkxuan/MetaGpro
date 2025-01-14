d95d475c69a62d6199af9e8d98de728e093bc12e
rkxuan <332931271@qq.com>
2025-01-14 15:14:38 +0800

    pretrain models

---
+++ prompt_graph/pretrain/__init__.py
@@ -0,0 +1,8 @@
+from.Edgepred_GPPT import Edgepred_GPPT
+from.Edgepred_Gprompt import Edgepred_Gprompt
+from.GraphCL import GraphCL
+from.SimGRACE import SimGRACE
+from.MultiGPrompt import NodePrePrompt, GraphPrePrompt, prompt_pretrain_sample
+from.GraphMAE import GraphMAE
+from.DGI import DGI
+from.ADGCL import GInfoMinMax
